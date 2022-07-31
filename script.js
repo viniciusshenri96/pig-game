7fa453b874a4c3b2a9a4e91a70cb34ed20cbb012
viniciusshenri96 <vinicius_henrique96@hotmail.com>
2022-07-31 10:56:38 -0300

    Implementando a funcionalidade ao botão NEW GAME e finalizando o projeto

--- script.js
+++ script.js
@@ -14,14 +14,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// Starting conditions
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+};
+init();
 
 // Troca de jogadores
 const switchPlayer = function () {
@@ -60,7 +73,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       document
@@ -76,3 +89,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// Resetting the Game
+btnNew.addEventListener('click', init);
